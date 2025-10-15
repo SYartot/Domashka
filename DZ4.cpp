843c4d34230ccf47f67a95cd7ce2e1557820af7b
SYartot <salivonyaroslav0@gmail.com>
2025-10-15 20:30:28 +0000

    DZ4.cpp

--- DZ4.cpp
+++ DZ4.cpp
@@ -1,5 +1,29 @@
-void numberToString() {
-    int num = 12345;
-    string str = to_string(num);
-    cout << "Number " << num << " converted to string: \"" << str << "\"" << endl;
+#include <iostream>
+
+const int a = 124125;
+int b;
+int c = a;		
+int i;		
+int g;			
+
+int main()
+{
+  char x[20];	
+  char y[20];		
+  while (c > 0)		
+  {
+	b = c % 10;
+	c = c / 10;
+	x[i] = '0'+b;
+	i++;
+  }
+  int p = i;		
+  for (g = 0; g < p; g++)
+  {
+	y[g] = x[i-1];
+	i--;
+  }
+  y[g] = '\0';		
+
+  std::cout << y;
 }
